1f886b58e7ff4efe7e9783149b0fcc0c1598f9df
florentromero-sketch <florent.romero@gmail.com>
2025-10-15 20:13:14 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,60 +1,24 @@
-const CACHE_NAME = 'jtt-portal';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'jtt-v3.6';
+const FILES_TO_CACHE = [
   '/jtt-portal/',
   '/jtt-portal/index.html',
-  '/jtt-portal/icon-192.png',
-  '/jtt-portal/icon-512.png',
-  '/jtt-portal/manifest.json',
-  '/jtt-portal/service-worker.js',
-  'https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap',
-  'https://cdn.jsdelivr.net/npm/chart.js'
+  '/jtt-portal/manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-      .catch((error) => {
-        console.error('Erreur lors du cache:', error);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return fetch(event.request)
-          .then((response) => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          });
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(response => response || fetch(event.request))
   );
 });
